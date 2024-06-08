4be87d082455397dce186d45eaad73064fb36ba5
MateusADantas <mahawprogramacao@gmail.com>
2024-06-08 13:23:47 -0300

    project setup

---
+++ src/styles.ts
@@ -0,0 +1,27 @@
+import { createGlobalStyle } from "styled-components";
+
+export const colors = {
+  brown: "#B77409",
+  black: "#000000",
+  gray: "#F1F1F2 ",
+  white: "#FFFFFF",
+  yellow: "#FFC42E ",
+};
+
+export const breakpoints = {
+  desktop: "1366px",
+  phone: "768px",
+};
+
+export const GlobalCss = createGlobalStyle`
+  * {
+    margin: 0px;
+    padding: 0px;
+    box-sizing: border-box;
+    font-family: 'Roboto', sans-serif;
+    list-style: none;
+    scrollbar-color: ${colors.gray} transparent;
+    scrollbar-width: thin;
+    text-decoration: none;
+  }
+`;
