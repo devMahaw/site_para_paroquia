9b44a29ea9e3c4c69bb2ef4fab2ae4894001940b
MateusADantas <mahawprogramacao@gmail.com>
2024-06-29 16:34:36 -0300

    feat: batismo section almost finished

--- src/pages/Batismo/index.tsx
+++ src/pages/Batismo/index.tsx
@@ -54,7 +54,7 @@ const Batismo = () => {
             até a sua prática na nossa vida diária. Assim, compreenderemos a sua
             importância em nossa vida.
           </S.SmallText>
-          <S.BigText>O que é um sacramento?</S.BigText>
+          <S.BigText id="oque-sacramento">O que é um sacramento?</S.BigText>
           <S.SmallText>
             Os sacramentos são sinais eficazes da graça divina, Jesus Cristo os
             estabeleceu e os confiou à Igreja para transmitirem a Sua
@@ -92,7 +92,9 @@ const Batismo = () => {
             fundamentais para a nossa salvação, uma vez que nos unem a Cristo e
             à Sua obra redentora.
           </S.SmallText>
-          <S.BigText>O que é o sacramento do Batismo?</S.BigText>
+          <S.BigText id="oque-batismo">
+            O que é o sacramento do Batismo?
+          </S.BigText>
           <S.SmallText>
             O batismo é o primeiro Sacramento, onde você é iniciado na vida
             cristã, você só pode receber os outros Sacramentos caso tenha o
@@ -211,7 +213,7 @@ const Batismo = () => {
           </S.SmallText>
           <S.SmallText>
             Além disso, o Batismo — assim como a Confirmação — imprime na alma
-            do Cristão um sinal espiritual indelével de pertença a Cristo &nbsp;
+            do Cristão um sinal espiritual indelével de pertença a Cristo&nbsp;
             <sup>
               <a
                 href="https://www.vatican.va/archive/cathechism_po/index_new/p2s2cap1_1210-1419_po.html"
@@ -220,16 +222,425 @@ const Batismo = () => {
                 10
               </a>
             </sup>
-            , por isso só se pode receber o sacramento do Batismo uma vez na
-            vida. Aliás, Santo Tomás em sua catequese sobre os sacramentos
-            afirma que o erro dos donatistas era rebatizar aqueles já batizados
-            pelos católicos “contra os quais se diz: Uma só fé, um só batismo (
+            , por isso{" "}
+            <b>só se pode receber o sacramento do Batismo uma vez na vida</b>.
+            Aliás, Santo Tomás em sua catequese sobre os sacramentos afirma que
+            o erro dos donatistas era rebatizar aqueles já batizados pelos
+            católicos “contra os quais se diz: Uma só fé, um só batismo (
             <a href="https://www.bibliaonline.com.br/acf/ef/4/5" target="blank">
               Ef 4,5
             </a>
             ).”
           </S.SmallText>
+          <S.DefaultText
+            style={{
+              padding: "30px 0px",
+              fontWeight: "bold",
+              fontSize: "30px",
+            }}
+          >
+            Quem pode batizar?
+          </S.DefaultText>
+          <S.SmallText>
+            Em situações normais, o Batismo é administrado pelos ministros
+            ordenados da Igreja, como padres e diáconos. No entanto, em
+            circunstâncias de emergência, qualquer pessoa pode realizar o
+            Batismo — até um pagão — desde que tenha a intenção correta e siga a
+            fórmula trinitária. Isso ocorre porque o Batismo é considerado
+            essencial para a salvação, e a igreja reconhece a vontade de Deus de
+            que todos se salvem.&nbsp;
+            <sup>
+              <a
+                href="https://www.vatican.va/archive/cathechism_po/index_new/p2s2cap1_1210-1419_po.html"
+                target="blank"
+              >
+                11
+              </a>
+            </sup>
+          </S.SmallText>
+          <S.SmallText>
+            Nesse sentido, uma vez que o Batismo é necessário para a nossa
+            salvação, a Igreja confia à misericórdia de Deus as crianças que
+            morrem sem receber este sacramento.&nbsp;
+            <sup>
+              <a
+                href="https://www.vatican.va/archive/cathechism_po/index_new/p2s2cap1_1210-1419_po.html"
+                target="blank"
+              >
+                12
+              </a>
+            </sup>
+            &nbsp;“De facto, a grande misericórdia de Deus […] e a ternura de
+            Jesus para com as crianças […] permitem-nos esperar que haja um
+            caminho de salvação para as crianças que morrem sem Baptismo.”&nbsp;
+            <sup>
+              <a
+                href="https://www.vatican.va/archive/cathechism_po/index_new/p2s2cap1_1210-1419_po.html"
+                target="blank"
+              >
+                13
+              </a>
+            </sup>
+          </S.SmallText>
+          <S.SmallText>
+            Um erro dos pelagianos, aponta Santo Tomás na sua catequese sobre os
+            sacramentos, era acreditar que as crianças batizadas são admitidas
+            no reino de Deus como alguém que passa de um estado bom a um melhor,
+            e não como alguém que foi liberada de um mal ao qual estava ligada.
+            Por isso, é forte o apelo da Igreja para que não adiem ou impeçam o
+            Batismo das crianças, em qualquer circunstância, sobretudo nas
+            situações emergenciais.
+          </S.SmallText>
+          <S.BigText id="batismo-biblia">
+            Sacramento do Batismo na Bíblia
+          </S.BigText>
+          <S.SmallText>
+            O Sacramento do Batismo encontra suas raízes na Bíblia e na história
+            cristã, tendo sua origem no ministério de Jesus Cristo. No entanto,
+            na Antiga Aliança, já se podiam ver indícios do Batismo em momentos
+            marcantes. A travessia do Mar Vermelho, bem como a história da arca
+            de Noé e a passagem do Jordão foram eventos que apontaram para a
+            salvação trazida pelo Batismo.&nbsp;
+            <sup>
+              <a
+                href="https://www.vatican.va/archive/cathechism_po/index_new/p2s2cap1_1210-1419_po.html"
+                target="blank"
+              >
+                14
+              </a>
+            </sup>
+            &nbsp;Além disso, a água, símbolo de vida, foi sendo preparada ao
+            longo do tempo para manifestar a graça do Batismo.&nbsp;
+            <sup>
+              <a
+                href="https://www.vatican.va/archive/cathechism_po/index_new/p2s2cap1_1210-1419_po.html"
+                target="blank"
+              >
+                15
+              </a>
+            </sup>
+          </S.SmallText>
+          <S.SmallText>
+            Tais prefigurações encontram a sua <b>realização em Jesus Cristo</b>
+            , que dá início à sua vida pública ao ser batizado por São João
+            Batista no rio Jordão, deixando um exemplo e uma base para o
+            sacramento&nbsp;
+            <sup>
+              <a
+                href="https://www.vatican.va/archive/cathechism_po/index_new/p2s2cap1_1210-1419_po.html"
+                target="blank"
+              >
+                16
+              </a>
+            </sup>
+            &nbsp;. Assim, Jesus também demonstrou Sua humildade e inaugurou uma
+            nova fase da história da redenção.
+          </S.SmallText>
+          <S.SmallText>
+            Nesse sentido, a Páscoa de Cristo abriu as fontes do Batismo para
+            toda a humanidade, e Seu sangue e água simbolizaram o Batismo e a
+            Eucaristia como sacramentos de vida nova.&nbsp;
+            <sup>
+              <a
+                href="https://www.vatican.va/archive/cathechism_po/index_new/p2s2cap1_1210-1419_po.html"
+                target="blank"
+              >
+                17
+              </a>
+            </sup>
+            &nbsp; Ademais, Ele confiou aos Seus discípulos a missão de batizar
+            todas as nações em nome do Pai e do Filho e do Espírito
+            Santo.”&nbsp;
+            <sup>
+              <a
+                href="https://www.bibliaonline.com.br/acf/mt/28/19"
+                target="blank"
+              >
+                18
+              </a>
+            </sup>
+          </S.SmallText>
+          <S.SmallText>
+            E os apóstolos, obedientes ao Mestre, começaram a praticar o Batismo
+            logo após o Dia de Pentecostes. Pedro, ao pregar à multidão em
+            Jerusalém, declarou: “Arrependei-vos, e cada um de vós seja batizado
+            em nome de Jesus Cristo para perdão dos pecados, e recebereis o dom
+            do Espírito Santo.”&nbsp;
+            <sup>
+              <a
+                href="https://www.bibliaonline.com.br/acf/atos/2/38"
+                target="blank"
+              >
+                19
+              </a>
+            </sup>
+            &nbsp;O Batismo, então, tornou-se uma cerimônia de entrada na
+            comunidade cristã e um{" "}
+            <b>sinal visível de pertencimento à família de Deus</b>.
+          </S.SmallText>
+          <S.SmallText>
+            O Apóstolo Paulo também enfatizou o significado profundo deste
+            sacramento, dizendo que ao sermos batizados, participamos da morte e
+            ressurreição de Cristo, sendo sepultados com Ele no Batismo e
+            ressurgindo para uma nova vida.&nbsp;
+            <sup>
+              <a
+                href="https://www.bibliaonline.com.br/nvi/rm/6/3,4"
+                target="blank"
+              >
+                20
+              </a>
+            </sup>
+          </S.SmallText>
+          <S.BigText id="batismo-celebrado">
+            Como é celebrado o sacramento do Batismo?
+          </S.BigText>
+          <S.SmallText>
+            O rito se inicia com o sinal da cruz que “manifesta a marca de
+            Cristo impressa naquele que vai passar a pertencer-Lhe, e […] a
+            graça da redenção que Cristo nos adquiriu pela sua cruz.”&nbsp;
+            <sup>
+              <a
+                href="https://www.vatican.va/archive/cathechism_po/index_new/p2s2cap1_1210-1419_po.html"
+                target="blank"
+              >
+                21
+              </a>
+            </sup>
+          </S.SmallText>
+          <S.SmallText>
+            A matéria do Batismo é a água, que é um símbolo profundo de
+            purificação, vida e renovação espiritual.&nbsp;
+            <sup>
+              <a
+                href="https://www.vatican.va/archive/cathechism_po/index_new/p2s2cap1_1210-1419_po.html"
+                target="blank"
+              >
+                22
+              </a>
+            </sup>
+            &nbsp;Essa água é consagrada por uma oração de epiclese, na qual a
+            Igreja invoca o Espírito Santo sobre a água, tornando-a um veículo
+            de graça divina.&nbsp;
+            <sup>
+              <a
+                href="https://www.vatican.va/archive/cathechism_po/index_new/p2s2cap1_1210-1419_po.html"
+                target="blank"
+              >
+                23
+              </a>
+            </sup>
+            &nbsp;Nesse momento, a água torna-se não apenas um elemento físico,
+            mas um canal sagrado de transformação espiritual.
+          </S.SmallText>
+          <S.SmallText>
+            Durante o rito, o óleo dos catecúmenos também é aplicado antes do
+            Batismo, como um sinal de proteção espiritual contra o mal, enquanto
+            o santo crisma é usado após o Batismo, simbolizando a presença do
+            Espírito Santo e a incorporação em Cristo. Esses óleos carregam
+            significados profundos de preparação espiritual e compromisso com a
+            fé.&nbsp;
+            <sup>
+              <a
+                href="https://www.vatican.va/archive/cathechism_po/index_new/p2s2cap1_1210-1419_po.html"
+                target="blank"
+              >
+                24
+              </a>
+            </sup>
+          </S.SmallText>
+          <S.SmallText>
+            O momento central do rito do Batismo envolve a imersão do candidato
+            na água ou a aspersão da água sobre sua cabeça enquanto o ministro
+            pronuncia a fórmula trinitária “N., eu te batizo em nome do Pai, e
+            do Filho, e do Espírito Santo.”&nbsp;
+            <sup>
+              <a
+                href="https://www.vatican.va/archive/cathechism_po/index_new/p2s2cap1_1210-1419_po.html"
+                target="blank"
+              >
+                25
+              </a>
+            </sup>
+            &nbsp;Tal gesto simboliza a morte para o pecado e o renascimento em
+            uma nova vida em Cristo. Aqui, a matéria e a forma se entrelaçam: a
+            matéria é a água, e a forma consiste nas palavras pronunciadas pelo
+            ministro.
+          </S.SmallText>
+          <S.SmallText>
+            Ao pronunciar essa fórmula, o ministro confere o caráter sacramental
+            ao ato, selando a participação do candidato na vida divina e na
+            comunidade de fé. Nesse momento, a graça do Batismo é infundida,
+            removendo o pecado original e inaugurando a vida divina na alma do
+            batizado.&nbsp;
+            <sup>
+              <a
+                href="https://www.vatican.va/archive/cathechism_po/index_new/p2s2cap1_1210-1419_po.html"
+                target="blank"
+              >
+                26
+              </a>
+            </sup>
+            &nbsp;Essa graça, além de purificação, capacita o batizado a viver
+            de acordo com os princípios cristãos e a desempenhar um papel ativo
+            na comunidade da Igreja.&nbsp;
+            <sup>
+              <a
+                href="https://www.vatican.va/archive/cathechism_po/index_new/p2s2cap1_1210-1419_po.html"
+                target="blank"
+              >
+                27
+              </a>
+            </sup>
+          </S.SmallText>
+          <S.BigText id="batismo-diaria">
+            Sacramento do Batismo na nossa vida diária
+          </S.BigText>
+          <S.SmallText>
+            Papa Bento XVI, em sua homilia da Festa do Batismo do Senhor,
+            enfatizou que o Batismo é mais do que uma mera fórmula ritual; é uma
+            adoção e incorporação na família divina, uma união com a Santíssima
+            Trindade. “Estas palavras [a forma] não são apenas uma fórmula, mas
+            uma realidade. Assinalam o momento em que os vossos filhos renascem
+            como filhos de Deus. De filhos de pais humanos que são, passam a ser
+            também filhos de Deus no Filho de Deus vivo.”&nbsp;
+            <sup>
+              <a
+                href="https://www.vatican.va/content/benedict-xvi/pt/homilies/2007/documents/hf_ben-xvi_hom_20070107_battesimo.html"
+                target="blank"
+              >
+                28
+              </a>
+            </sup>
+          </S.SmallText>
+          <S.SmallText>
+            O Batismo é o sacramento que abre as portas para os demais. É o
+            início da vida de fé, o começo de uma vida em sintonia com a nova
+            identidade de filho de Deus. Por isso, desde a infância, os
+            padrinhos e pais têm o papel de guiar os batizados nesse
+            caminho&nbsp;
+            <sup>
+              <a
+                href="https://www.vatican.va/archive/cathechism_po/index_new/p2s2cap1_1210-1419_po.html"
+                target="blank"
+              >
+                29
+              </a>
+            </sup>
+            , a fim de que cresçam em conhecimento e fé até a Confirmação e
+            perseverem.
+          </S.SmallText>
+          <S.SmallText>
+            Após o Batismo, somos exortados a “viver de acordo com o
+            Espírito”&nbsp;
+            <sup>
+              <a
+                href="https://www.vatican.va/archive/cathechism_po/index_new/p2s2cap1_1210-1419_po.html"
+                target="blank"
+              >
+                30
+              </a>
+            </sup>
+            &nbsp;, ou seja, adotar um estilo de vida guiado pelo Espírito
+            Santo. Sendo assim, a vivência dos frutos do Espírito — amor,
+            alegria, paz, paciência, autodomínio, entre outros&nbsp;
+            <sup>
+              <a
+                href="https://www.bibliaonline.com.br/nvi/gl/5/22,23"
+                target="blank"
+              >
+                31
+              </a>
+            </sup>
+            &nbsp;— expressa a atuação do Espírito Santo em nossas vidas como
+            batizados.
+          </S.SmallText>
+          <S.SmallText>
+            Por fim, ao nos tornarmos membros da Igreja, deixamos de pertencer
+            apenas a nós mesmos e passamos a pertencer àquele que se sacrificou
+            e ressurgiu por nós — Cristo. Nesse sentido, somos chamados a nos
+            submeter aos outros, servir em comunhão na Igreja e a demonstrar
+            obediência e humildade diante da hierarquia eclesial. Assim como o
+            Batismo traz consigo responsabilidades e deveres, também nos confere
+            direitos, como receber os sacramentos, nutrir-nos com a Palavra de
+            Deus e ser amparados por outros auxílios espirituais.
+          </S.SmallText>
+          <S.DefaultText style={{ fontWeight: "bold", margin: "30px 0px" }}>
+            Referências
+          </S.DefaultText>
+          <S.SmallText
+            style={{
+              paddingBottom: "30px",
+              borderBottom: "1px solid #D4D4D4",
+            }}
+          >
+            <span>1. CIC, 1257</span>
+            <br />
+            <span>2, 3. CIC, 1131</span>
+            <br /> <span>4, 5. CIC, 1213</span>
+            <br /> <span>6. CIC, 1214</span>
+            <br />
+            <span>7. CIC, 1215</span>
+            <br /> <span>8. CIC, 1270</span>
+            <br /> <span>9. CIC, 1246</span>
+            <br />
+            <span>10. CIC, 1317</span>
+            <br /> <span>11. CIC, 1256</span>
+            <br />
+            <span>12, 13. CIC, 1261</span>
+            <br />
+            <span>14. CIC 1218-1220</span>
+            <br />
+            <span>15. CIC 1217</span>
+            <br />
+            <span>16. CIC 1223</span>
+            <br />
+            <span>17. CIC 1225</span>
+            <br />
+            <span>18. Mt 28, 19</span>
+            <br />
+            <span>19. Atos 2, 38</span>
+            <br />
+            <span>20. Rm 6,3-4</span>
+            <br />
+            <span>21. CIC, 1235</span>
+            <br />
+            <span>22. CIC, 1262</span>
+            <br />
+            <span>23. CIC, 1238</span>
+            <br />
+            <span>24. CIC, 1239</span>
+            <br />
+            <span>25. CIC, 1240</span>
+            <br />
+            <span>26. CIC, 1279</span>
+            <br />
+            <span>27. CIC, 1269</span>
+            <br />
+            <span>
+              28. Homilia do Papa Bento XVI na Festa do Baptismo do Senhor,
+              janeiro de 2007
+            </span>
+            <br />
+            <span>29. CIC, 1255</span>
+            <br />
+            <span>30. CIC 1266</span>
+            <br />
+            <span>31. Gl 5, 22-23</span>
+          </S.SmallText>
         </div>
+        <S.LinksContainer>
+          <h4>O que você vai encontrar neste artigo?</h4>
+          <a href="#oque-sacramento">O que é um sacramento?</a>
+          <a href="#oque-batismo">O que é o sacramento do Batismo?</a>
+          <a href="#batismo-biblia">Sacramento do Batismo na Bíblia</a>
+          <a href="#batismo-celebrado">
+            Como é celebrado o sacramento do Batismo?
+          </a>
+          <a href="#batismo-diaria">
+            Sacramento do Batismo na nossa vida diária
+          </a>
+        </S.LinksContainer>
       </div>
     </div>
   );
