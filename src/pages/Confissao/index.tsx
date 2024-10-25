edef081d4cdea2d71edcf846fe9854d902817f81
MateusADantas <mahawprogramacao@gmail.com>
2024-10-24 21:40:08 -0300

    feat: confissao section continuing

--- src/pages/Confissao/index.tsx
+++ src/pages/Confissao/index.tsx
@@ -47,6 +47,200 @@ const Confissao = () => {
             nossa vida. Além disso, examinaremos suas raízes bíblicas e o rito
             que o envolve.
           </S.SmallText>
+          <S.BigText id="oque-confissao">
+            O que é o sacramento da Confissão?
+          </S.BigText>
+          <S.SmallText>
+            O Sacramento da Confissão é o sacramento instituído por Jesus Cristo
+            para perdoar os pecados cometidos após o Batismo.
+            <sup>
+              <a
+                href="https://www.vatican.va/archive/cathechism_po/index_new/p2s2cap1_1420-1532_po.html"
+                target="blank"
+              >
+                2
+              </a>
+            </sup>
+            Ele oferece aos fiéis a oportunidade de se arrependerem de seus
+            pecados, confessá-los a um sacerdote e receber o perdão divino.{" "}
+            <strong>
+              O arrependimento sincero e a contrição perfeita são partes
+              fundamentais da confissão
+            </strong>
+            , uma vez que a verdadeira conversão do coração é essencial para a
+            validade do sacramento.
+            <sup>
+              <a
+                href="https://www.vatican.va/archive/cathechism_po/index_new/p2s2cap1_1420-1532_po.html"
+                target="blank"
+              >
+                3
+              </a>
+            </sup>
+          </S.SmallText>
+          <S.SmallText>
+            O processo de reconciliação começa com um exame de consciência, por
+            meio do qual os fiéis refletem sobre suas ações e reconhecem os
+            pecados cometidos. Em seguida, eles procuram um sacerdote em um
+            momento de confissão, a fim de relatar os seus pecados. Ao final, o
+            confessor impõe ao fiel uma penitência a ser feita em reparação
+            pelos pecados cometidos. O sacerdote age como um intermediário entre
+            o penitente e Deus, conferindo a absolvição dos pecados por meio do
+            poder recebido de Cristo.
+            <sup>
+              <a
+                href="https://www.vatican.va/archive/cathechism_po/index_new/p2s2cap1_1420-1532_po.html"
+                target="blank"
+              >
+                4
+              </a>
+            </sup>
+          </S.SmallText>
+          <S.SmallText>
+            A absolvição é uma ação que vai além do mero perdão humano. O
+            sacerdote, agindo “in persona Christi”, concede a absolvição
+            sacramental,
+            <strong>
+              libertando o penitente da culpa espiritual e restaurando sua
+              amizade com Deus e com a Igreja
+            </strong>
+            . Esse ato é um testemunho tangível do amor incondicional de Deus e
+            da Sua misericórdia infinita.
+            <sup>
+              <a
+                href="https://www.vatican.va/archive/cathechism_po/index_new/p2s2cap1_1420-1532_po.html"
+                target="blank"
+              >
+                5
+              </a>
+            </sup>
+          </S.SmallText>
+          <S.SmallText>
+            O Sacramento da Confissão é um caminho de cura e renovação interior,
+            através do qual os católicos podem experimentar o amor e o perdão de
+            Deus. Ele oferece a oportunidade de reconhecer a própria
+            fragilidade, arrepender-se sinceramente e ser restaurado na graça
+            divina. É, portanto, uma fonte de graça e de crescimento espiritual
+            que nos permite avançar na vida de fé.
+            <sup>
+              <a
+                href="https://www.vatican.va/archive/cathechism_po/index_new/p2s2cap1_1420-1532_po.html"
+                target="blank"
+              >
+                6
+              </a>
+            </sup>
+          </S.SmallText>
+          <S.BigText id="confissao-biblia">
+            Sacramento da Confissão na Bíblia
+          </S.BigText>
+          <S.SmallText>
+            O Sacramento da Confissão tem raízes bíblicas e reflete a contínua
+            vontade de Deus de reconciliar-se com a humanidade. No livro dos
+            Salmos, o rei Davi expressa profundos sentimentos de arrependimento
+            e busca pelo perdão após seu pecado com Betsabeia.
+            <sup>
+              <a
+                href="https://www.bibliaonline.com.br/nvi/sl/51"
+                target="blank"
+              >
+                7
+              </a>
+            </sup>
+            Além disso, o Antigo Testamento apresenta a prática de oferecer
+            sacrifícios de expiação como forma de reconciliação com Deus. O
+            livro do Levítico descreve rituais específicos para lidar com
+            determinados pecados e impurezas, enfatizando a importância de
+            buscar purificação e restauração.
+            <sup>
+              <a
+                href="https://www.bibliaonline.com.br/nvi/lv/15/1-33"
+                target="blank"
+              >
+                8
+              </a>
+            </sup>
+          </S.SmallText>
+          <S.SmallText>
+            O Novo Testamento revela que só Deus tem o poder de perdoar pecados;
+            Jesus, o Filho de Deus encarnado, manifesta essa autoridade durante
+            seu ministério terreno. Ele perdoa pecados ao curar um paralítico e
+            afirma ser capaz de fazê-lo como Filho do Homem.
+            <sup>
+              <a
+                href="https://www.bibliaonline.com.br/acf/mc/2/5-10"
+                target="blank"
+              >
+                9
+              </a>
+            </sup>
+            No entanto, após Sua ressurreição, Jesus confere também aos
+            apóstolos a autoridade de perdoar os pecados.
+          </S.SmallText>
+          <S.SmallText>
+            No Evangelho de João, Jesus sopra sobre os discípulos e diz:
+            “Recebei o Espírito Santo; a quem perdoardes os pecados, lhes são
+            perdoados; a quem os retiverdes, lhes são retidos”.
+            <sup>
+              <a
+                href="https://www.bibliaonline.com.br/nvi/jo/20/21-23"
+                target="blank"
+              >
+                10
+              </a>
+            </sup>
+            Isso estabelece o alicerce para o Sacramento da Confissão como um
+            meio de reconciliação contínua.
+            <sup>
+              <a
+                href="https://www.vatican.va/archive/cathechism_po/index_new/p2s2cap1_1420-1532_po.html"
+                target="blank"
+              >
+                11
+              </a>
+            </sup>
+          </S.SmallText>
+          <S.SmallText>
+            Ademais, Jesus lhes dá a autoridade de reconciliar os pecadores com
+            a Igreja. Essa dimensão eclesial do ministério apostólico é
+            destacada nas palavras de Jesus a Pedro sobre as chaves do Reino dos
+            Céus: “Tudo o que ligares na terra será ligado nos céus, e tudo o
+            que desligares na terra será desligado nos céus.”
+            <sup>
+              <a
+                href="https://www.bibliaonline.com.br/acf/mt/16/19"
+                target="blank"
+              >
+                12
+              </a>
+            </sup>
+            . E esse poder é também concedido ao colégio apostólico.
+            <sup>
+              <a
+                href="https://www.vatican.va/archive/cathechism_po/index_new/p2s2cap1_1420-1532_po.html"
+                target="blank"
+              >
+                13
+              </a>
+            </sup>
+          </S.SmallText>
+          <S.SmallText>
+            Tais palavras indicam que aqueles excluídos da comunhão eclesial
+            também ficam excluídos da comunhão com Deus, enquanto aqueles
+            reintegrados na comunidade também são acolhidos por Deus. Pois “a
+            reconciliação com a Igreja é inseparável da reconciliação com Deus.”
+            <sup>
+              <a
+                href="https://www.vatican.va/archive/cathechism_po/index_new/p2s2cap1_1420-1532_po.html"
+                target="blank"
+              >
+                14
+              </a>
+            </sup>
+          </S.SmallText>
+          <S.BigText id="celebracao-reconciliacao">
+            Celebração do Sacramento da Reconciliação
+          </S.BigText>
         </S.TextContainer>
       </S.Container>
     </div>
