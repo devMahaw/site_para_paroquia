59ad448b66b97f9078342d4824c4e108079f14b0
Mahaw <mahawprogramacao@gmail.com>
2024-11-02 04:10:18 -0300

    feat: home section continuing

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -2,7 +2,20 @@ import * as S from "../../styles";
 
 const Home = () => {
   return (
-    <div style={{ margin: "50px" }}></div>
+    <div>
+      <div>
+      <img style={{borderRadius: "20px", objectFit: "cover", display: "block", margin: "120px auto" }} width={"1400px"} src="assets/nswallpaper.jpg" alt="Nossa Senhora do Perpétuo Socorro" />
+      </div>
+      <div style={{display: "flex", flexDirection: "column", alignItems: "center", justifyContent: "center", marginTop: "-40px"}}>
+        <S.DefaultText style={{letterSpacing: "5px", fontSize: "26px"}}>CONHEÇA</S.DefaultText>
+        <S.BigText style={{ fontSize: "50px", marginTop: "25px"}}>NOSSAS ATIVIDADES</S.BigText>
+        <div style={{display: "flex"}}>
+          <div></div>
+          <div></div>
+          <div></div>
+        </div>
+      </div>
+    </div>
   )
 };
 
